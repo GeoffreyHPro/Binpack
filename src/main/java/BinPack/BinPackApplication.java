255fd7ba8238070a085193aa18a9027612f93b77
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-02-14 00:42:56 +0100

    feat: add api rest project

---
+++ src/main/java/BinPack/BinPackApplication.java
@@ -0,0 +1,13 @@
+package BinPack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinPackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BinPack/BinPackApplicationTests.java
@@ -0,0 +1,13 @@
+package BinPack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinPackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
